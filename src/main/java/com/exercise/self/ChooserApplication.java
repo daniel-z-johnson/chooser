51a83785946acf8300da20e52cedc42dd0c324e4
prime23 <danzaljohnson@gmail.com>
2016-10-04 02:29:04 -0600

    Initial commit

---
+++ src/main/java/com/exercise/self/ChooserApplication.java
@@ -0,0 +1,12 @@
+package com.exercise.self;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChooserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChooserApplication.class, args);
+	}
+}
